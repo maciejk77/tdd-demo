87f86c8ea8a5d133d6b58b0ba67f67037d1ffabf
Maciej Kurek <maciejk77@gmail.com>
2019-10-10 00:05:48 +0100

    started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  const app = wrapper.find("[data-test='app']");
+  expect(app.length).toBe(1);
 });
+
+it('renders increment button', () => {});
+
+it('renders counter display', () => {});
+
+it('counter starts at 0', () => {});
+
+it('clicking button increments the counter display', () => {});
