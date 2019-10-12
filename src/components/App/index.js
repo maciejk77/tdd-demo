7aaa86dd1efe96db52f84501d6f887d166f03ead
Maciej Kurek <maciejk77@gmail.com>
2019-10-12 19:04:08 +0100

    getMatchCount function

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Congrats from '../Congrats';
+import GuessedWords from '../GuessedWords';
 
-const App = () => (
-  <div>
-    <Congrats />
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[
+            { guessedWord: 'train', letterMatchCount: 3 },
+            { guessedWord: 'party', letterMatchCount: 5 }
+          ]}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
